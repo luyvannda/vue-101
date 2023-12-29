7c9483427c659510de65543d92126e5e39d97370
luyvannda <philips.joe329@gmail.com>
2023-12-29 15:50:32 +0700

    learn about v-bind in Vue

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "A pair of socks are what we need to get through a cold winter night."
+      image: './assets/images/socks_green.jpg',
+      url: "https://github.com/luyvannda"
     }
   }
 })
