9aa2732b4116cac05515b0b758410d5fab57d35a
luyvannda <philips.joe329@gmail.com>
2023-12-30 10:09:04 +0700

    complete list rendering tutorial

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      product: "Socks",
-      image: './assets/images/socks_green.jpg',
-      inventory: 100,
-      onSale: true
+      product: 'Socks',
+      image: './assets/images/socks_blue.jpg',
+      inStock: true,
+      details: ['50% cotton', '30% wool', '20% polyester'],
+      variants: [
+        { id: 2234, color: 'green' },
+        { id: 2235, color: 'blue' }
+      ],
+      sizes: [
+        "S", "M", "L", "XL", "XXL"
+      ]
     }
   }
 })
