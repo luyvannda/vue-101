ef82198c811ddcedadbfc6d5148e5157a2d995c2
luyvannda <philips.joe329@gmail.com>
2023-12-30 09:38:02 +0700

    complete vue tutorial on conditional rendering

--- main.js
+++ main.js
@@ -3,7 +3,8 @@ const app = Vue.createApp({
     return {
       product: "Socks",
       image: './assets/images/socks_green.jpg',
-      url: "https://github.com/luyvannda"
+      inventory: 100,
+      onSale: true
     }
   }
 })
