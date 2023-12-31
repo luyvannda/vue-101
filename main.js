ffcb7bf64f7977f3285513c424f9f7de4745c3dc
luyvannda <philips.joe329@gmail.com>
2023-12-31 11:03:26 +0700

    move product display into a new component and import it back to the index.html

--- main.js
+++ main.js
@@ -1,48 +1,10 @@
 const app = Vue.createApp({
   data() {
     return {
-      cart: 0,
-      product: 'Socks',
-      brand: 'Vue Mastery',
-      selectedVariant: 0,
-      onSale: true,
-      details: ['50% cotton', '30% wool', '20% polyester'],
-      variants: [
-        { id: 2234, color: 'green', image: './assets/images/socks_green.jpg', quantity: 50 },
-        { id: 2235, color: 'blue', image: './assets/images/socks_blue.jpg', quantity: 0 },
-      ]
+      cart: 0
     }
   },
-  methods: {
-    addToCart() {
-      this.cart += 1
-    },
-    updateVariant(index) {
-      this.selectedVariant = index
-    }
-  },
+  methods: {}
 
-  computed: {
-    title() {
-      return this.brand + ' ' + this.product
-    },
-
-    image() {
-      return this.variants[this.selectedVariant].image
-    },
-    inStock() {
-      return this.variants[this.selectedVariant].quantity
-    },
-
-    promotion() {
-      if (this.inStock <= 0) {
-        this.onSale = false
-        return ""
-      } else if (this.inStock > 0) {
-        this.onSale = true
-        return this.title + " is on sale"
-      }
-    }
-  }
 
 })
