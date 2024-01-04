b40379fd431b496b3aa606dbbe7764af8a2f802b
luyvannda <philips.joe329@gmail.com>
2024-01-04 15:40:11 +0700

    learn to export and import app component

---
+++ components/AppButton.js
@@ -0,0 +1,23 @@
+export default {
+  template: /*html*/
+    `
+            <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" 
+            :disabled="processing"
+            >
+            <slot />
+              
+            </button>    
+  
+            `,
+
+  data() {
+    return {
+      processing: false
+    };
+  }
+
+}
+
+
+
+
