42dcc832a44e649e31d4f8a27500c6c59c16ae6b
luyvannda <philips.joe329@gmail.com>
2024-01-05 10:42:59 +0700

    initialize new lessons

--- components/App.js
+++ components/App.js
@@ -1,7 +1,64 @@
-import AppButton from "./AppButton.js"
 
 export default {
   components: {
-    'app-button': AppButton
+
+    template:
+      /*html*/
+      `
+      <section v-if="inProgressAssignments.length">
+
+        <h2 class="font-bold mb-2">In Progress</h2>
+
+        <ul>
+          <li
+              v-for="assignment in inProgressAssignments"
+              :key="assignment.id">
+            <label>
+              {{ assignment.name }}
+
+              <input type="checkbox" v-model="assignment.complete">
+            </label>
+          </li>
+        </ul>
+
+      </section>
+
+      <section class="mt-8" v-if="completedAssignments.length">
+
+        <h2 class="font-bold mb-2">Completed</h2>
+
+        <ul>
+          <li v-for="assignment in completedAssignments"
+              :key="assignment.id">
+            <label>
+              {{ assignment.name }}
+
+              <input type="checkbox" v-model="assignment.complete">
+            </label>
+          </li>
+        </ul>
+
+      </section>
+
+    `,
+    data() {
+      return {
+        assignments: [
+          { id: 1, name: 'Finish project', complete: false },
+          { id: 2, name: 'Read chapter 4', complete: false },
+          { id: 3, name: 'Turn in homework', complete: false }
+        ]
+      }
+    },
+
+    computed: {
+      completedAssignments() {
+        return this.assignments.filter(a => a.complete)
+      },
+
+      inProgressAssignments() {
+        return this.assignments.filter(a => !a.complete)
+      }
+    }
   }
 };
